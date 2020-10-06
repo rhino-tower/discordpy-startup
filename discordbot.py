5dd61f6bfd065035dd70b3c14ea7e0595eff9eae
rhino-tower <69337200+rhino-tower@users.noreply.github.com>
2020-10-07 03:49:32 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,7 +1,11 @@
 import discord
 import os
+import discord.ext import tasks
+import datetime
+import locale
 
 TOKEN = os.environ['DISCORD_BOT_TOKEN']
+CHANNEL_ID = os.environ['DISCORD_CHANNEL_ID']
 
 # 接続に必要なオブジェクトを生成
 client = discord.Client()
@@ -18,6 +22,14 @@ async def on_message(message):
         return
     if message.content == 'これからよろしく！':
         await message.channel.send('了解しました！！')
+@tasks.loop(seconds = 86400)
+async def loop():
+    dt = datetime.datetime.now()
 
+    if dt.strftime('%A') == 'Monday':
+        channel = client.get_channel(int(CHANNEL_ID))
+        await channel.send('今日からソフトウェア技術とサイバー技術の課題が出ます！\nお忘れなく!')
+
+loop.start()
 # Botの起動とDiscordサーバーへの接続
 client.run(TOKEN)
