09af57cc7713fcbcfb57c4db7de62714fac05db1
rhino-tower <69337200+rhino-tower@users.noreply.github.com>
2020-10-08 23:54:03 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,21 +1,45 @@
-from discord.ext import commands
-import os
-import traceback
+import os  
+import traceback  
 
-bot = commands.Bot(command_prefix='/')
-token = os.environ['DISCORD_BOT_TOKEN']
+import discord  
+from discord.ext import commands  
 
+from modules.grouping import MakeTeam  
 
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
+token = os.environ['DISCORD_BOT_TOKEN']  
+bot = commands.Bot(command_prefix='/')  
 
+"""起動処理"""  
+@bot.event  
+async def on_ready():  
+    print('-----Logged in info-----')  
+    print(bot.user.name)  
+    print(bot.user.id)  
+    print(discord.__version__)  
+    print('------------------------')  
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
+"""コマンド実行"""  
+# メンバー数が均等になるチーム分け  
+@bot.command()  
+async def team(ctx, specified_num=2):  
+    make_team = MakeTeam()  
+    remainder_flag = 'true'  
+    msg = make_team.make_party_num(ctx,specified_num,remainder_flag)  
+    await ctx.channel.send(msg)  
 
+# メンバー数が均等にはならないチーム分け  
+@bot.command()  
+async def team_norem(ctx, specified_num=2):  
+    make_team = MakeTeam()  
+    msg = make_team.make_party_num(ctx,specified_num)  
+    await ctx.channel.send(msg)  
 
-bot.run(token)
+# メンバー数を指定してチーム分け  
+@bot.command()  
+async def group(ctx, specified_num=1):  
+    make_team = MakeTeam()  
+    msg = make_team.make_specified_len(ctx,specified_num)  
+    await ctx.channel.send(msg)  
+
+"""botの接続と起動"""  
+bot.run(token) 
