98b9ac59025d3e73b64848c17d626ecdffca3a4e
rhino-tower <69337200+rhino-tower@users.noreply.github.com>
2020-10-10 23:56:58 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -11,29 +11,10 @@ bot = commands.Bot(command_prefix = '/')
 client = discord.Client()
 
 @client.event
-async def on_ready():
-    msg = "やあ！TEST Botです．よろしくね！"
-    await client.send_message(text_chat,msg)
-
-@client.event
 async def on_message(message):
     if message.author.bot:
         return
     if message.content == 'これからよろしく！':
         print("了解しました‼")
 
-@tasks.loop(seconds = 10)
-async def loop():
-    channel = client.get_channel(int(CHANNEL_ID))
-    await channel.send('みんな何してるんだー⁇')
-"""
-@tasks.loop(seconds = 86400)
-async def loop():
-    dt = datetime.datetime.now()
-
-    if dt.strftime('%A') == 'Monday':
-        channel = client.get_channel(int(CHANNEL_ID))
-        await channel.send('今日からソフトウェア技術とサイバー技術の課題が出ます！\nお忘れなく!')
-"""
-loop.start()
 bot.run(TOKEN)
