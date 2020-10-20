e71fe3cf59ca390e320b40138de4acb721a319b0
rhino-tower <69337200+rhino-tower@users.noreply.github.com>
2020-10-21 01:09:40 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -25,7 +25,7 @@ def time_limit_msg(date_dict):
     M = int(date_dict['minute'])
 
     dt_deadline = datetime(year=y, month=m, day=d, hour=h, minute=M)
-    dt_now = datetime.now(tmiezone('Asia/Tokyo'))
+    dt_now = datetime.now(timezone('Asia/Tokyo'))
     left_time = dt_deadline - dt_now
     
     if dt_deadline < dt_now:
